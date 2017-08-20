145e2671c74a3de7eb44d2ce2657c674498cb9d2
Hyeonu Park <nemo1275@gmail.com>
2017-08-20 13:19:45 +0900

    start v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,2 @@
 #[macro_use]
 extern crate nom;
-
-extern crate unicode_segmentation;
-
-pub mod ast;
-pub mod parser;
--- src/parser/mod.rs
+++
@@ -1,9 +0,0 @@
-use ast::front::*;
-
-mod ident;
-mod sep;
-mod literal;
-
-pub fn parse(src: &str) -> Result<Ast<Module>, ()> {
-    unimplemented!();
-}
