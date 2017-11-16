e35093aecd89d58c44c3d3901cf282a213c963c1
Hyeonu Park <nemo1275@gmail.com>
2017-11-16 10:48:24 +0900

    add comment syntax
    Nal language has only one kind of comment syntax: //
    This is intentional, see
    http://lpar.ath0.com/2010/12/31/multi-line-comments-considered-harmful/

--- nal_ast/src/parse/common.rs
+++ nal_ast/src/parse/common.rs
@@ -4,22 +4,33 @@ pub type Input<'src> = LocatedSpan<&'src str>;
 
 pub fn noop<T>(_: T) {}
 
+named!(newline(Input) -> (), alt_complete!(
+    map!(
+        tuple!(tag!("//"), is_not_s!("\n"), tag!("\n")),
+        noop
+    ) |
+    map!(
+        tag!("\n"),
+        noop
+    )
+));
+
 named!(pub sp(Input) -> (), map!(
   fold_many0!(is_a!(" \t\r"), (), |_, _| ()),
-  |_| ()
+  noop
 ));
 
 named!(pub sp_f(Input) -> (), map!(
   tuple!(is_a!(" \t"), sp),
-  |_| ()
+  noop
 ));
 
 named!(pub nl(Input) -> (), map!(
-  tuple!(sp, fold_many0!(tuple!(tag!("\n"), sp), (), |_, _| ())),
-  |_| ()
+  tuple!(sp, fold_many0!(tuple!(newline, sp), (), |_, _| ())),
+  noop
 ));
 
 named!(pub nl_f(Input) -> (), map!(
-  tuple!(sp, fold_many1!(tuple!(tag!("\n"), sp), (), |_, _| ())),
-  |_| ()
+  tuple!(sp, fold_many1!(tuple!(newline, sp), (), |_, _| ())),
+  noop
 ));
